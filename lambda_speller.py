dedeb28dd05fe39f379da06ec0e8a704db7d6b03
Ubuntu <ubuntu@ip-172-31-42-209.us-west-2.compute.internal>
2019-05-14 10:31:07 +0000

    Adding initial code

---
+++ lambda_speller.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+
+from random import shuffle
+import boto3
+import botocore
+def lambda_handler(event,context):
+  s3 = boto3.resource('s3')
+  obj = s3.Object('rs-97-emulator-helper','text.txt')
+  in_file = obj.get()['Body'].read().decode('utf-8')
+  print in_file
+  words = in_file.splitlines()
+  text = '<speak>'
+  text += ' Hello <lang xml:lang="es-MX">Agustin</lang>, these are your spelling words for this week.<break time="3s"/>'
+    
+  shuffle(words)
+
+  i = 0
+  while i < len(words):
+      break_time = len(words[i])
+      text += words[i]+', <break time="2s"/>'+words[i]+'<break time="'+str(break_time)+'s"/>,'
+      i += 1
+
+  text +='</speak>'
+
+
+  polly = boto3.client('polly')
+
+  response = polly.synthesize_speech(VoiceId='Joanna',
+                  OutputFormat='mp3',
+                  TextType='ssml',
+                  LanguageCode='en-GB', 
+                  Text = text)
+
+  recording = response['AudioStream'].read()
+
+  s3 = boto3.resource('s3')
+  audio = s3.Object('rs-97-emulator-helper','speech.mp3')
+  audio.put(Body=recording)
+  return {
+    "Text": text
+  }
---
+++ local_speller.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+from random import shuffle
+import boto3
+
+words = []
+text = '<speak>'
+text += ' Hello <lang xml:lang="es-MX">Agustin</lang>, these are your spelling words for this week.<break time="3s"/>'
+with open('myfile.txt') as fp:
+  for line in fp:
+    line.rstrip()
+    words.append(line)
+    
+shuffle(words)
+
+i = 0
+while i < len(words):
+    break_time = len(words[i])
+    text += words[i]+', <break time="2s"/>'+words[i]+'<break time="'+str(break_time)+'s"/>,'
+    i += 1
+
+text +='</speak>'
+
+print text
+
+polly = boto3.client('polly')
+
+response = polly.synthesize_speech(VoiceId='Joanna',
+                OutputFormat='mp3',
+                TextType='ssml',
+                LanguageCode='en-GB', 
+                Text = text)
+
+recording = response['AudioStream'].read()
+
+s3 = boto3.resource('s3')
+audio = s3.Object('rs-97-emulator-helper','speech.mp3')
+audio.put(Body=recording)
+
+#file = open('speech.mp3', 'w')
+#file.write(response['AudioStream'].read())
+#file.close()
